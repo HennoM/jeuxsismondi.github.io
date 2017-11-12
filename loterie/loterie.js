afbc0e25b97d6e944e96ba346aa04a83c6a0a53f
jeuxsismondi <33466232+jeuxsismondi@users.noreply.github.com>
2017-11-12 15:40:34 +0100

    Update loterie.js

--- loterie/loterie.js
+++ loterie/loterie.js
@@ -1,1 +1,21 @@
-Math.floor((Math.random() * 100) + 1);
+var nombreMystere = Math.floor((Math.random() * 100) + 1);
+alert(nombreMystere);
+var msgVue = new Vue({
+  el: '#textArea',
+  data: {
+    message: 'Hello Vue.js!'
+  }
+})
+function guess() {
+    var guessedNumber = document.getElementById("userInput").value;
+    alert(guessedNumber);
+    
+    if (guessedNumber == nombreMystere) {
+        msgVue.message = 'WP';
+    } else if (guessedNumber > nombreMystere) {
+        msgVue.message = 'Your number is superior';
+    } else {
+        msgVue.message = 'Your number is inferior';
+    }
+}
+
