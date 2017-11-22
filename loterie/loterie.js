773e9fca0c4f38d4770a03929327f55f36e9af0f
jeuxsismondi <33466232+jeuxsismondi@users.noreply.github.com>
2017-11-22 11:31:16 +0100

    Add files via upload

--- loterie/loterie.js
+++ loterie/loterie.js
@@ -1,26 +1,26 @@
 var nombreMystere = Math.floor((Math.random() * 100) + 1);
-alert(nombreMystere);
-var msgVue = new Vue({
-  el: '#textArea',
-  data: {
-    message: 'Entrez un nombre entre 0 et 100 compris'
-  }
-})
 
 function guess() {
     var guessedNumber = document.getElementById("userInput").value;
-    alert(guessedNumber);
-    
+
     if (guessedNumber == nombreMystere) {
-        msgVue.message = "Bien joué !";
+      document.getElementById("textArea").innerHTML = "Bien joué !";
     } else if (guessedNumber > nombreMystere) {
-        msgVue.message = "C'est moins !";
-    } else {
-        msgVue.message = "C'est plus !";
+        document.getElementById("textArea").innerHTML = "C'est moins !";
+    } else if (guessedNumber < nombreMystere) {
+        document.getElementById("textArea").innerHTML = "C'est plus !";
+    } else if (guessedNumber > 100) { //ne fonctionne pas
+      document.getElementById("textArea").innerHTML = "Veuillez entrer un nombre inférieur ou égal à 100";
+    } else if (guessedNumber < 0) { //ne fonctionne pas
+      document.getElementById("textArea").innerHTML = "Veuillez entrer un nombre supérieur ou égal à 0";
     }
 }
 
-function restart (nombreMystere) {
-    var nombreMystere = Math.floor((Math.random() * 100) + 1);
-    alert(nombreMystere);
+function restart () {
+    nombreMystere = Math.floor((Math.random() * 100) + 1);
+    guess();
 }
+
+//Ajouter nb d'essais (1) et nombres déjà entrés (2) + changer bouton "Entrer" pour la touche enter (3)
+//Arrêter le jeu qd nombre mystère trouvé + gérer entrées utilisateur (interdire lettres)
+//Remettre à zéro input qd "recommencer"
