ab86700e8408119e377706367bb49426798674f8
jeuxsismondi <33466232+jeuxsismondi@users.noreply.github.com>
2018-01-09 22:46:35 +0100

    Update loterie.js

--- Loterie/loterie.js
+++ Loterie/loterie.js
@@ -36,7 +36,7 @@ function guess() {
         answerVue.message = "C'est moins !";
         lives -= 1;
         livesVue.message = "Lives: " + lives.toString();
-    } else {
+    } else if (guessedNumber < nombreMystere) {
         answerVue.message = "C'est plus !";
         lives -= 1;
         livesVue.message = "Vies: " + lives.toString();
